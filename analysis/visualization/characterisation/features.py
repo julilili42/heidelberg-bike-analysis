f2a83770a08684f6e9f8534ce065fb2c11f6fa39
Simon Rappenecker <simi2657@gmail.com>
2025-12-26 23:41:00 +0100

    added another plot to compare holiday assignment

--- analysis/visualization/characterisation/features.py
+++ analysis/visualization/characterisation/features.py
@@ -1,13 +1,16 @@
 from analysis.visualization.characterisation.helpers import find_peak
-from analysis.visualization.characterisation.indices import (hourly_index, monthly_index)
+from analysis.visualization.characterisation.indices import hourly_index, monthly_index
 import polars as pl
 import numpy as np
 
-def build_feature_df(loader, interval=None):
+
+def build_feature_df(loader, interval=None, filter_dates=None, neg_dates=False):
     rows = []
 
     for station in loader.get_bicyle_stations():
-        feats = calc_feature_vector(loader, station, interval)
+        feats = calc_feature_vector(
+            loader, station, interval, filter_dates=filter_dates, neg_dates=neg_dates
+        )
 
         row = {"station": station}
         if feats is None:
@@ -21,30 +24,51 @@ def build_feature_df(loader, interval=None):
     return pl.DataFrame(rows)
 
 
+def calc_feature_vector(
+    loader, station_name, interval=None, filter_dates=None, neg_dates=False
+):
+    Ih_weekday = hourly_index(
+        loader=loader,
+        station_name=station_name,
+        interval=interval,
+        weekday=True,
+        filter_dates=filter_dates,
+        neg_dates=neg_dates,
+    )
+    Ih_weekend = hourly_index(
+        loader=loader,
+        station_name=station_name,
+        interval=interval,
+        weekday=False,
+        filter_dates=filter_dates,
+        neg_dates=neg_dates,
+    )
+    Im = monthly_index(
+        loader=loader,
+        station_name=station_name,
+        interval=interval,
+        filter_dates=filter_dates,
+        neg_dates=neg_dates,
+    )
 
-def calc_feature_vector(loader, station_name, interval=None, filter_dates=None, neg_dates=False):
-    Ih_weekday = hourly_index(loader=loader, station_name=station_name, interval=interval, weekday=True, filter_dates=filter_dates, neg_dates=neg_dates)
-    Ih_weekend = hourly_index(loader=loader, station_name=station_name, interval=interval, weekday=False, filter_dates=filter_dates, neg_dates=neg_dates)
-    Im = monthly_index(loader=loader, station_name=station_name, interval=interval, filter_dates=filter_dates, neg_dates=neg_dates)
-    
-    # seasons must be required, station which does not exist for given interval => height = 0 
+    # seasons must be required, station which does not exist for given interval => height = 0
     # we want to cluster all stations which do not exist for given interval with None
-    if (Im.filter(pl.col("month").is_in([6,7,8])).height == 0 or 
-        Im.filter(pl.col("month").is_in([11,12,1,2])).height == 0):
+    if (
+        Im.filter(pl.col("month").is_in([6, 7, 8])).height == 0
+        or Im.filter(pl.col("month").is_in([11, 12, 1, 2])).height == 0
+    ):
         return None
 
     dpi = double_peak_index(Ih=Ih_weekday)
     wsd = weekend_shape_diff_index(Ih_wd=Ih_weekday, Ih_we=Ih_weekend)
     sdi = seasonal_drop_index(Im=Im)
-    
-    return {
-        "DPI": dpi, 
-        "WSD": wsd,
-        "SDI": sdi
-    }
+
+    return {"DPI": dpi, "WSD": wsd, "SDI": sdi}
 
 
 """ FEATURES """
+
+
 def seasonal_drop_index(Im):
     q90 = Im["I_m"].quantile(0.9)
     q10 = Im["I_m"].quantile(0.1)
@@ -52,8 +76,8 @@ def seasonal_drop_index(Im):
 
 
 def double_peak_index(Ih):
-    h_m, p_m = find_peak(Ih, 5, 10)     # first peak 
-    h_e, p_e = find_peak(Ih, 14, 20)    # second peak
+    h_m, p_m = find_peak(Ih, 5, 10)  # first peak
+    h_e, p_e = find_peak(Ih, 14, 20)  # second peak
 
     midday = (
         Ih.filter((pl.col("hour") >= 8) & (pl.col("hour") < 14))
@@ -80,6 +104,3 @@ def weekend_shape_diff_index(Ih_wd, Ih_we):
     p_we = p_we / p_we.sum()
 
     return float(np.linalg.norm(p_wd - p_we))
-
-
-
